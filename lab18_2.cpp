5421f1359b3c8cc90a9ed6412a1636188308151c
Ef <phurin.ef@gmail.com>
2026-02-13 16:55:12 +0700

    lab18 complete

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,20 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
 
-
-
+double overlap(Rect *R1,Rect *R2){
+    double left,right,top,bottom;
+    if(R1->x>R2->x) left=R1->x;
+	else left=R2->x;
+    if(R1->x+R1->w>R2->x+R2->w)right=R2->x+R2->w;
+	else right=R1->x+R1->w;
+    if(R1->y+R1->h<R2->y+R2->h)top=R1->y;
+	else top=R2->y;
+    if(R1->y-R1->h<R2->y-R2->h)bottom=R2->y-R2->h;
+	else bottom=R1->y-R1->h;
+    double x_over=right-left;
+    double y_over=top-bottom;
+    double area_over= x_over*y_over;
+    if(area_over<0)return 0;
+    else return area_over;
 }
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -8,9 +8,11 @@
 using namespace std;
 
 struct student{
+    string name;
+    int id;
+    char gender;
+    float gpa;
 
-    //[Missing Code 1] Define struct student with four members (name ,id , gender, gpa);
-    
 };
 
 struct course{
@@ -33,17 +35,16 @@ student text2student(string text){
         else if(count == 3) gpa += text[i];
     }
     
-    //[Missing Code 2] Fill in the blank with the correct code.;
     s.name = name;
-    s.id = _____________;
-    s.gender = _____________;
-    s.gpa = _____________;
+    s.id = stoi(id);
+    s.gender = gen[0];
+    s.gpa = stof(gpa);
     
-    _____________;
+    return s;
 }
 
 
-student * findstudent(vector<student> allstudents,int key){ //[Missing Code 4] There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //[Missing Code 4] There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -103,7 +104,7 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
+			    allcourses[allcourses.size()-1].lecture_list.push_back(textline);
 			    //[Missing Code 3] Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
 			    
 			}			
@@ -112,7 +113,7 @@ int main(){
 				state = 1;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
+				allcourses[allcourses.size()-1].student_list.push_back(p);
 				//[Missing Code 5] Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
